0e1ebfffded5459bf2b33e028e2768e234d7076b
Glitch (purring-neighborly-hero) <none>
2021-08-12 19:34:44 +0000

    ☄️🍂 Checkpoint ./package.json:16231546/4975 ./server.js:16231546/196

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
+// Basic Configuration
 require('dotenv').config()
 
 app.use(cors())
@@ -9,10 +14,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
+app.use(bodyParser.json());
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+// Connect to Database
