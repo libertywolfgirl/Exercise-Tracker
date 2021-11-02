ec3dac92732c87ff0f0fdcb8b9d9dcebd5baae84
Glitch (purring-neighborly-hero) <none>
2021-11-02 22:12:31 +0000

    🕵🐓 Checkpoint ./server.js:16231546/266 ./public/script.js:16231546/24

--- server.js
+++ server.js
@@ -97,11 +97,9 @@ app.post("/api/users", async function(req, res) {
 
 // Post exercise
 app.post("/api/users/:_id/exercises", async function(req, res) {
-  console.log(1);
   const { description, duration, date } = req.body;
   const { _id } = req.params;
   try {
-    console.log(2);
     const newDate = date
       ? new Date(date).toDateString()
       : new Date().toDateString();
@@ -110,7 +108,6 @@ app.post("/api/users/:_id/exercises", async function(req, res) {
       duration: parseInt(duration),
       date: newDate
     };
-    console.log(3);
     const findOne = await User.findByIdAndUpdate(
       {
         _id
@@ -124,7 +121,6 @@ app.post("/api/users/:_id/exercises", async function(req, res) {
         new: true
       }
     );
-    console.log(4);
     if (findOne) {
       const { username } = findOne;
 
